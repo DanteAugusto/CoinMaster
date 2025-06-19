8a5b4732543ea472e6020da212dae47a6a484951
Paulo Vitor Lima Borges <31678236+PauloVLB@users.noreply.github.com>
2025-06-18 22:10:53 -0300

    Staging (#50)
    * readme atualizado
    
    * feature(#3): inicializa projeto spring e adiciona estrutra básica
    
    * feature(#4): adiciona classe de modelo de Conta
    
    * feature(#5): funcionalidade de cadastrar conta desenvolvida
    
    * refactor(#5): renomeando metodo de endpoint de interface
    
    * feat(#7): adicionar credito implementado
    
    * feat(#7): adicionado arquivo de interface para feature
    
    * feat(#9): transferencia implementada
    
    * feat(#9): a conta agora pode ter saldo negativo depois de uma transferencia
    
    * feature(#6): endpoint de consultar saldo implementado
    
    * feature(#8): endpoint de debito implementado
    
    * feature(#17): adiciona instrucoes de uso e execucao no readme
    
    * fix(#18) : saldo nao pode mais ser negativo
    
    * feature(#22): adiciona conta bonus e funcionalidade relacionadas
    
    * feat(#23: ContaPoupanca e juros implementados
    
    * fix(#26): valores devem ser positivos
    
    * feature(#31): cadastras poupanca requer saldo inicial
    
    * feat(#32) - limite maximo de saldo negativo de 1000
    
    * bugfix(#39): ajuste no valor para pontuacao em transferencia para conta bonus
    
    * feature(42): consultar conta
    
    * refactor(#43): refatoracao geral
    
    * refactor(#43): remove paginas html
    
    * wip: primeiros testes implementados
    
    * wip: teste de credito consertado
    
    * feat: testes unitarios finalizados
    
    ---------
    
    Co-authored-by: Dante <dante.augusto.bezerra.129@ufrn.edu.br>
    Co-authored-by: Dante Augusto <43281304+DanteAugusto@users.noreply.github.com>

--- coinmaster/src/main/java/gcm/coinmaster/controller/ContaController.java
+++ coinmaster/src/main/java/gcm/coinmaster/controller/ContaController.java
@@ -4,90 +4,90 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import gcm.coinmaster.model.Conta;
 import gcm.coinmaster.model.ContaPoupanca;
 import gcm.coinmaster.model.ContaBonus;
+import gcm.coinmaster.model.DTO.ContaDTO;
 import gcm.coinmaster.model.DTO.TransferenciaDTO;
 import gcm.coinmaster.service.ContaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
-@RequestMapping("/contas")
+@RequestMapping("/banco/conta")
 public class ContaController {
 
     @Autowired
     private ContaService contaService;
 
-    @PostMapping("/cadastrar")
+    @PostMapping
     public ResponseEntity<Conta> cadastrarConta(@RequestParam String numero, @RequestParam Double saldo) {
         Conta conta = contaService.cadastrarConta(numero, saldo);
         return ResponseEntity.ok(conta);
     }
 
-    @PostMapping("/cadastrarPoupanca")
-    public ResponseEntity<ContaPoupanca> cadastrarContaPoupanca(@RequestParam String numero) {
-        ContaPoupanca contaPoupanca = contaService.cadastrarContaPoupanca(numero);
+    @PostMapping("/poupanca")
+    public ResponseEntity<ContaPoupanca> cadastrarContaPoupanca(@RequestParam String numero, @RequestParam(required = false) Double saldoInicial) {
+        ContaPoupanca contaPoupanca = contaService.cadastrarContaPoupanca(numero, saldoInicial);
         return ResponseEntity.ok(contaPoupanca);
     }
 
-    @PostMapping("/renderJuros")
-    public ResponseEntity<ContaPoupanca> renderJuros(@RequestParam String numero, @RequestParam double taxa) { //taxa = 10 quer dizer 10%
+    @PutMapping("/{numero}/rendimento")
+    public ResponseEntity<ContaPoupanca> renderJuros(@PathVariable String numero, @RequestParam double taxa) { //taxa = 10 quer dizer 10%
         ContaPoupanca contaPoupanca;
         contaPoupanca = contaService.renderJuros(numero, taxa);
         return ResponseEntity.ok(contaPoupanca);
     }
 
-    @PostMapping("/renderJurosTodos")
+    @PutMapping("/rendimento")
     public ResponseEntity<List<ContaPoupanca>> renderJurosTodos(@RequestParam double taxa) {
         List<ContaPoupanca> contaPoupancas;
         contaPoupancas = contaService.renderJurosTodos(taxa);
         return ResponseEntity.ok(contaPoupancas);
     }
-    @PostMapping("/cadastrar-conta-bonus")
+    @PostMapping("/bonus")
     public ResponseEntity<Conta> cadastrarContaBonus(@RequestParam String numero) {
         ContaBonus conta = contaService.cadastrarContaBonus(numero);
         return ResponseEntity.ok(conta);
     }
 
-    @GetMapping("/cadastrar-conta")
-    public String cadastrarContaInterface() {
-        return "cadastrar-conta";
-    }
-
-    @PostMapping("/transferencia")
+    @PutMapping("/transferencia")
     public ResponseEntity<TransferenciaDTO> fazerTransferencia(@RequestParam String origem, @RequestParam String destino, @RequestParam double valor) {
         TransferenciaDTO transferenciaDTO = contaService.fazerTransferencia(origem, destino, valor);
         return ResponseEntity.ok(transferenciaDTO);
     }
 
-    @GetMapping("/fazer-transferencia")
-    public String fazerTransferenciaInterface() {
-        return "fazer-transferencia";
-    }
-    
-    @PostMapping("/credito")
-    public ResponseEntity<Conta> porCredito(@RequestParam String numero, @RequestParam double credito) {
+    @PutMapping("/{numero}/credito")
+    public ResponseEntity<Conta> porCredito(@PathVariable String numero, @RequestParam double credito) {
         Conta conta = contaService.porCredito(numero, credito);
         return ResponseEntity.ok(conta);
     }
 
-    @GetMapping("/por-credito")
-    public String porCreditoInterface() {
-        return "por-credito";
-    }
-
-    @GetMapping("/consultar-saldo")
-    public ResponseEntity<Double> consultarSaldo(@RequestParam String numero) {
+    @GetMapping("/{numero}/saldo")
+    public ResponseEntity<Double> consultarSaldo(@PathVariable String numero) {
         double saldo = contaService.consultarSaldo(numero);
         return ResponseEntity.ok(saldo);
     } 
 
-    @PostMapping("/debito")
-    public ResponseEntity<Conta> debito(@RequestParam String numero, @RequestParam double valor) {
+    @PutMapping("/{numero}/debito")
+    public ResponseEntity<Conta> debito(@PathVariable String numero, @RequestParam double valor) {
         Conta conta = contaService.debito(numero, valor);
         return ResponseEntity.ok(conta);
     }
-}
 
+    @GetMapping("/{numero}")
+    public ResponseEntity<ContaDTO> consultarConta(@PathVariable String numero) {
+        ContaDTO conta = contaService.consultarConta(numero);
+        return ResponseEntity.ok(conta);
+    }
+
+    @GetMapping
+    public List<ContaDTO> consultarContas() {
+        List<ContaDTO> contas = contaService.consultarContas();
+        return contas;
+    }
+    
+}
