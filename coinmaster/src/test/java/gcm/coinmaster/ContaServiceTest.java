4fb9bed4c4e7d40e131c5ce9e7879fd780937af1
Dante <dante.augusto.bezerra.129@ufrn.edu.br>
2025-06-18 21:53:38 -0300

    feat: testes unitarios finalizados

--- coinmaster/src/test/java/gcm/coinmaster/ContaServiceTest.java
+++ coinmaster/src/test/java/gcm/coinmaster/ContaServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Nested;
@@ -36,7 +37,7 @@ public class ContaServiceTest {
             Double saldo = 1000.0;
             Conta conta = new Conta(numero, saldo);
             when(contaRepository.save(any())).thenReturn(conta);
-            assertEquals(conta, contaService.cadastrarConta(numero, saldo));
+            assertEquals(new Conta(numero, saldo), contaService.cadastrarConta(numero, saldo));
         }
 
         @Test
@@ -47,7 +48,11 @@ public class ContaServiceTest {
             contaBonus.setSaldo(0.0);
             contaBonus.setPontuacao(10);
             when(contaRepository.save(any())).thenReturn(contaBonus);
-            assertEquals(contaBonus, contaService.cadastrarContaBonus(numero));
+            ContaBonus contaBonus2 = new ContaBonus();
+            contaBonus2.setNumero(numero);
+            contaBonus2.setSaldo(0.0);
+            contaBonus2.setPontuacao(10);
+            assertEquals(contaBonus2, contaService.cadastrarContaBonus(numero));
         }
 
         @Test
@@ -58,7 +63,10 @@ public class ContaServiceTest {
             contaPoupanca.setNumero(numero);
             contaPoupanca.setSaldo(saldo);
             when(contaRepository.save(any())).thenReturn(contaPoupanca);
-            assertEquals(contaPoupanca, contaService.cadastrarContaPoupanca(numero, saldo));
+            ContaPoupanca contaPoupanca2 = new ContaPoupanca();
+            contaPoupanca2.setNumero(numero);
+            contaPoupanca2.setSaldo(saldo);
+            assertEquals(contaPoupanca2, contaService.cadastrarContaPoupanca(numero, saldo));
         }
 
     }
@@ -72,8 +80,9 @@ public class ContaServiceTest {
             double saldo = 1000.0;
             Conta conta = new Conta(numero, saldo);
             ContaDTO contaDTO = new ContaDTO(conta);
+            ContaDTO contaDTO2 = new ContaDTO(conta);
             when(contaRepository.findById(any())).thenReturn(Optional.of(conta));
-            assertEquals(contaDTO, contaService.consultarConta(numero));
+            assertEquals(contaDTO2, contaService.consultarConta(numero));
         }
 
         @Test
@@ -84,8 +93,9 @@ public class ContaServiceTest {
             contaBonus.setSaldo(0.0);
             contaBonus.setPontuacao(10);
             ContaDTO contaDTO = new ContaDTO(contaBonus);
+            ContaDTO contaDTO2 = new ContaDTO(contaBonus);
             when(contaRepository.findById(any())).thenReturn(Optional.of(contaBonus));
-            assertEquals(contaDTO, contaService.consultarConta(numero));
+            assertEquals(contaDTO2, contaService.consultarConta(numero));
         }
 
         @Test
@@ -96,8 +106,9 @@ public class ContaServiceTest {
             contaPoupanca.setNumero(numero);
             contaPoupanca.setSaldo(saldo);
             ContaDTO contaDTO = new ContaDTO(contaPoupanca);
+            ContaDTO contaDTO2 = new ContaDTO(contaPoupanca);
             when(contaRepository.findById(any())).thenReturn(Optional.of(contaPoupanca));
-            assertEquals(contaDTO, contaService.consultarConta(numero));
+            assertEquals(contaDTO2, contaService.consultarConta(numero));
         }
 
     }
@@ -144,7 +155,7 @@ public class ContaServiceTest {
             contaBonus.setSaldo(saldo);
             contaBonus.setPontuacao(0);
             when(contaRepository.findById(any())).thenReturn(Optional.of(contaBonus));
-            assertEquals(contaBonus, contaService.porCredito(numero, credito));            
+            assertEquals(credito/100, (ContaBonus) contaService.porCredito(numero, credito));            
         }
     }
 
@@ -168,9 +179,9 @@ public class ContaServiceTest {
             assertEquals(null, contaService.debito(numero, debito));            
         }
 
-        // O saldo pode ficar até -1000.0, pois é o limite de crédito
+        // O saldo pode ficar até -1000.0, pois é o limite máximo de saldo negativo 
         @Test
-        void TestarDebitoMaiorQueSaldoMaisMil(){
+        void TestarDebitoSaldoNegativoMenosMilContaSimples(){
             String numero = "12345";
             double saldo = 500.0;
             double debito = 12000.0;
@@ -178,11 +189,65 @@ public class ContaServiceTest {
             when(contaRepository.findById(any())).thenReturn(Optional.of(conta));
             assertEquals(saldo, contaService.debito(numero, debito).getSaldo());            
         }
+
+        @Test
+        void TestarDebitoNaoPodeSaldoNegativoNaContaPoupanca(){
+            String numero = "12345";
+            double saldo = 500.0;
+            double debito = 501.0;
+            ContaPoupanca conta = new ContaPoupanca();
+            conta.setNumero(numero);
+            conta.setSaldo(saldo);
+            when(contaRepository.findById(any())).thenReturn(Optional.of(conta));
+            assertEquals(saldo, contaService.debito(numero, debito).getSaldo());            
+        }
     }
     
     @Nested
-    class Transferencia{}
+    class Transferencia{
+
+        @Test
+        void TestarTransferenciaNegativa() {
+            String numero = "12345";
+            double valor = -200.0;
+            assertEquals(null, contaService.fazerTransferencia(numero, numero, valor));    
+        }
+
+        @Test
+        void TestarTransferenciaSaldoNegativoMenosMilContaSimples() {
+            String numero = "12345";
+            double saldo = 500.0;
+            double valor = 12000.0;
+            Conta conta = new Conta(numero, saldo);
+            when(contaRepository.findById(any())).thenReturn(Optional.of(conta));
+            assertEquals(saldo, contaService.fazerTransferencia(numero, numero, valor).getContaOrigem().getSaldo());    
+        }
+
+        @Test
+        void TestarTransferenciaNaoPodeSaldoNegativoNaContaPoupanca(){
+            String numero = "12345";
+            double saldo = 200.0;
+            double valor = 501.0;
+            ContaPoupanca conta = new ContaPoupanca();
+            conta.setNumero(numero);
+            conta.setSaldo(saldo);
+            when(contaRepository.findById(any())).thenReturn(Optional.of(conta));
+            assertEquals(saldo, contaService.fazerTransferencia(numero, numero, valor).getContaOrigem().getSaldo());
+        }
+    }
 
     @Nested
-    class RenderJuros{}
+    class RenderJuros{
+        @Test
+        void TestarRenderJurosTodos() {
+            ContaPoupanca conta = new ContaPoupanca();
+            double saldo = 10.0;
+            conta.setSaldo(saldo);                   
+            when(contaRepository.findAll()).thenReturn(List.of(conta));            
+            when(contaRepository.findById(any())).thenReturn(Optional.of(conta));
+            double taxa = 50.0;
+            List<ContaPoupanca> contaPoupancas = contaService.renderJurosTodos(taxa);
+            assertEquals(saldo+saldo*taxa/100, contaPoupancas.get(0).getSaldo());
+        }
+    }
 }
